33681f79e5e331cef4122d00a887918f639fb355
taahahussainkhan <taaha128@gmail.com>
2024-01-11 11:00:30 +0500

    pattern 03

---
+++ 1.Patterns/03.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r, c, i = 1, j;
+    cout << "Rows: ";
+    cin >> r;
+    cout << "Columns: ";
+    cin >> c;
+    while(i <= r){
+        j = 1;
+        while(j <= c){
+            cout <<" " << j << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
