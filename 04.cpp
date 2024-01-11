69b1f1fc82ef5ff4ca44643426a5db583fd620d7
taahahussainkhan <taaha128@gmail.com>
2024-01-11 11:12:07 +0500

    more patterns pattern 04 and 05 in c++

---
+++ 04.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r, c, i = 1, j;
+    cout << "Rows: ";
+    cin >> r;
+    cout << "Columns: ";
+    cin >> c;
+    while (i <= r){
+        j = 1;
+        while (j <= c){
+            cout << " " << c - j + 1 << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+
+
+    return 0;
+}
---
+++ 05.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r, c, i = 1, j;
+    cout << "Rows: ";
+    cin >> r;
+    cout << "Columns: ";
+    cin >> c;
+    while (i <= r)
+    {
+        j = 1;
+        while (j <= c)
+        {
+            cout << " " << r - i + 1 << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
