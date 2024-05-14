97d86dcfd32e36e07ec54bfbe87eefc79fe2f9ae
taahahussainkhan <taaha128@gmail.com>
2024-05-15 01:34:59 +0500

    Patterns from striver

---
+++ 1.Patterns/Strivers/01.cpp
@@ -0,0 +1,149 @@
+#include<iostream>
+using namespace std;
+void pattern1(int n)
+{
+    for(int i = 1 ; i <= n ; i++)
+    {
+        for(int j = 0 ; j < n ; j++)
+        {
+            cout << " *";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+void pattern2(int n)
+{
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j <= i ; j++)
+        {
+            cout << " *";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+void pattern3(int n)
+{
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j <= i ; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+void pattern4(int n)
+{
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j <= i ; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+void pattern5(int n)
+{
+    for(int i = 1 ; i <= n ; i++)
+    {
+        for(int j = n ; j >= i ; j--)
+        {
+            cout << " *";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+void pattern6(int n)
+{
+    for(int i = 1 ; i <= n ; i++)
+    {
+        for(int j = n ; j >= i  ; j--)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+void pattern7(int n)
+{
+    for(int i = 1 ; i <= n ; i++)
+    {
+        for(int j = n ; j >= i  ; j--)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+void pattern8(int n)
+{
+    for(int i = 0 ; i < n ; i++)
+    {
+        //Space
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout<< "  ";
+        }
+        //Star
+        for (int k = 0; k < (2*i+1); k++)
+        {
+            cout<< " *";
+        }
+        //Space
+        for (int l = 0; l < n-i-1; l++)
+        {
+            cout<< "  ";
+        }        
+        cout << endl;
+    }
+    cout << endl;
+}
+void pattern9(int n)
+{
+    for(int i = 1 ; i <= n ; i++)
+    {
+        //Space
+        for (int j = 1; j <= i; j++)
+        {
+            cout<< "  ";
+        }
+        //Star
+        // n = 5
+        for (int k = n+(n-1); k >= i  ; k--)
+        {
+            cout<< " *";
+        }
+        //Space
+        for (int l = 1; l <= i; l++)
+        {
+            cout<< " ";
+        }        
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n = 5;
+  
+    pattern1(n);
+    pattern2(n);
+    pattern3(n);
+    pattern4(n);
+    pattern5(n);
+    pattern6(n);
+    pattern7(n);
+    pattern8(n);
+    pattern9(n);
+
+}
